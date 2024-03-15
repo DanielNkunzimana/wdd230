2aa93e184b5632343f9d030357fedbcbc756589d
DanielNkunzimana <mcdanielgolden11@gmail.com>
2024-03-15 18:25:24 +0200

    Home Page Enhancement

--- week01/scripts/getDates.js
+++ week01/scripts/getDates.js
@@ -3,3 +3,33 @@ document.getElementById('year').textContent = new Date().getFullYear();
 
 // Get last modified date for second paragraph in footer
 document.getElementById('lastModified').textContent = "Last modified: " + document.lastModified;
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('menu-toggle');
+    const closeMenuBtn = document.getElementById('close-menu');
+    const navItems = document.querySelector('nav ul');
+
+    menuToggle.addEventListener('click', function() {
+        navItems.classList.toggle('show-menu');
+        menuToggle.style.display = 'none';
+        closeMenuBtn.style.display = 'block';
+    });
+
+    closeMenuBtn.addEventListener('click', function() {
+        navItems.classList.remove('show-menu');
+        closeMenuBtn.style.display = 'none';
+        menuToggle.style.display = 'block';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const darkModeToggle = document.getElementById('dark-mode-toggle');
+    const main = document.querySelector('main');
+
+    darkModeToggle.addEventListener('click', function() {
+        main.classList.toggle('dark-mode');
+    });
+});
+
+
