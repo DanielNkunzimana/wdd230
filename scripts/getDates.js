da527b57042706fa47bcfa86aea609438d3fb8c2
DanielNkunzimana <mcdanielgolden11@gmail.com>
2024-03-26 09:46:49 +0200

    add change

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -4,7 +4,24 @@ document.getElementById('year').textContent = new Date().getFullYear();
 // Get last modified date for second paragraph in footer
 document.getElementById('lastModified').textContent = "Last modified: " + document.lastModified;
 
+document.addEventListener('DOMContentLoaded', function() {
+    // Retrieve page visit count from localStorage or set it to 0 if not exists
+    let pageVisits = localStorage.getItem('pageVisits');
+    if (!pageVisits) {
+        pageVisits = 0;
+    } else {
+        pageVisits = parseInt(pageVisits);
+    }
 
+    // Update page visit count in the HTML
+    document.getElementById('page-visits').textContent = pageVisits;
+
+    // Increment page visit count and update localStorage
+    pageVisits++;
+    localStorage.setItem('pageVisits', pageVisits.toString());
+});
+
+// Toggle menu
 document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.getElementById('menu-toggle');
     const closeMenuBtn = document.getElementById('close-menu');
@@ -23,6 +40,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+// Toggle dark mode
 document.addEventListener('DOMContentLoaded', function() {
     const darkModeToggle = document.getElementById('dark-mode-toggle');
     const main = document.querySelector('main');
@@ -31,5 +49,3 @@ document.addEventListener('DOMContentLoaded', function() {
         main.classList.toggle('dark-mode');
     });
 });
-
-
